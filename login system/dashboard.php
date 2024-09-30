f20de740dc616857efbaad01cd6547eb040b3caa
Sapna1921157 <sapnak7508@gmail.com>
2024-09-30 14:06:39 +0530

    added new updated in project

--- login system/dashboard.php
+++ login system/dashboard.php
@@ -12,6 +12,9 @@ if (!isset($_SESSION['user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <!-- Bootstrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/dashboard.css">
     <title>Dashboard</title>
 </head>
